622457f2e471ea3e553a29e8f62ad70f13200e93
jhosusruiz <jhosusruiz@gmail.com>
2025-11-05 18:38:00 -0500

    Update Arbitro.java
    subo codigo de la clase Arbitro

--- src/modelo/Arbitro.java
+++ src/modelo/Arbitro.java
@@ -1,4 +1,28 @@
 package modelo;
 
+/**
+ * Clase Arbitro 
+ * hereda de Persona (es una persona), pero con un rol especial.
+ * No tiene estadisticas ni dorsal, solo necesita su nombre para dirigir partidos.
+
+ */
 public class Arbitro extends Persona {
+    
+    /**
+     * Constructor - Un arbitro solo necesita su nombre
+     * @param nombre El nombre del arbitro
+     */
+    public Arbitro(String nombre) {
+        super(nombre);  // se pasa el nombre a Persona
+    }
+    
+    /**
+     * hay poliformismo
+     * Un arbitro muestra su informacion de manera mas sencilla que un jugador
+  
+     */
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Arbitro: " + nombre);
+    }
 }
