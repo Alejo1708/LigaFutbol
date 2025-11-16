28d95a3adcb3d2cd3c6925715a2363aa73de66c4
Alejo1708 <alejo20081701@gmail.com>
2025-11-15 22:28:33 -0500

    Update GestionArbitros.java
    Corrijo clase gestion arbitros

--- src/gestion/GestionArbitros.java
+++ src/gestion/GestionArbitros.java
@@ -1,4 +1,3 @@
-
 package gestion;
 
 import modelo.Persona;
@@ -9,6 +8,13 @@ import java.util.List;
 
 /**
  * Clase GestionArbitros - Administra a los arbitros de la liga
+ * 
+ * CORRECCIONES APLICADAS 
+ * 1. Se agrego el metodo agregarArbitro() para poder añadir arbitros a la gestion
+ * 2. Se agrego el método listarArbitros() para mostrar todos los arbitros
+ * 3. Se agrego el getter getPersonas() para acceder a la lista desde afuera
+ * 
+ * Ahora esta clase se usa desde Liga para gestionar arbitros de forma organizada
  */
 public class GestionArbitros {
     // Lista de personas (que pueden ser arbitros)
@@ -21,10 +27,21 @@ public class GestionArbitros {
         this.personas = new ArrayList<>();
     }
     
+    // CORRECCION 1: Método para agregar arbitros 
     /**
-     * Metodo para asignar un arbitro a un partido
-
-     * @param partido El partido al que se asignara un arbitro
+     * Agrega un arbitro a la gestion
+     * Este metodo permite añadir arbitros al sistema de gestion
+     * 
+     * @param arbitro El arbitro que se va a agregar
+     */
+    public void agregarArbitro(Arbitro arbitro) {
+        this.personas.add(arbitro);
+    }
+    
+    /**
+     * Metodo para asignar un arbitro a un partido, por ahora esta vacio porque los arbitros se asignan directamente en el constructor de Partido, pero podria tener logica automatica
+     * 
+     * @param partido El partido al que se le asignara un arbitro
      */
     public void asignarArbitro(Partido partido) {
         // Por ahora esta vacio porque se asignan arbitros directamente
@@ -32,6 +49,8 @@ public class GestionArbitros {
     
     /**
      * Elimina una persona (arbitro) del sistema por nombre
+     * Util si un arbitro se retira o ya no esta disponible
+     * 
      * @param nombre El nombre del arbitro a eliminar
      */
     public void eliminarPersona(String nombre) {
@@ -41,6 +60,8 @@ public class GestionArbitros {
     
     /**
      * Busca un arbitro especifico por su nombre
+     * Permite encontrar un arbitro en el sistema de gestion
+     * 
      * @param nombre El nombre del arbitro a buscar
      * @return El arbitro encontrado, o null si no existe
      */
@@ -52,5 +73,35 @@ public class GestionArbitros {
             }
         }
         return null;  // No se encontro 
+    }
+    
+    // CORRECCION 2: Metodo para listar arbitros 
+    /**
+     * Lista todos los arbitros registrados en el sistema
+     * Muestra en consola la informacion de cada arbitro
+     */
+    public void listarArbitros() {
+        System.out.println("-----LISTA DE ARBITROS-----");
+        if (personas.isEmpty()) {
+            System.out.println("No hay arbitros registrados");
+        } else {
+            for (Persona p : personas) {
+                if (p instanceof Arbitro) {
+                    p.mostrarInformacion();  // Usa el polimorfismo para mostrar informacion
+                }
+            }
+        }
+        System.out.println("________________________");
+    }
+    
+    //CORRECCION 3: Getter para acceder a la lista 
+    /**
+     * Obtiene la lista de personas (arbitros) del sistema
+     * Permite acceder a la lista desde otras clases
+     * 
+     * @return Lista de personas (arbitros)
+     */
+    public List<Persona> getPersonas() {
+        return personas;
     }
 }
