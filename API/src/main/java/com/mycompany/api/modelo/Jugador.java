a473fa14c5e257a0d9b90b1f9633e124f4081f64
Alejo1708 <alejo20081701@gmail.com>
2025-11-20 22:31:23 -0500

    Update Jugador.java
    subo el codigo de jugador

--- API/src/main/java/com/mycompany/api/modelo/Jugador.java
+++ API/src/main/java/com/mycompany/api/modelo/Jugador.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.api.modelo;
 
-/**
- *
- * @author Alejandro
- */
 public class Jugador {
-    
+    private String nombre;
+    private String posicion;
+    private int dorsal;
+
+    public Jugador(String nombre, String posicion, int dorsal) {
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.dorsal = dorsal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
 }
