1ae28b89d6f28e9d21bc1653ede08f2b23774390
jhosusruiz <jhosusruiz@gmail.com>
2025-11-20 23:38:29 -0500

    Update Estadio.java
    subo codigo estadio

--- API/src/main/java/com/mycompany/api/modelo/Estadio.java
+++ API/src/main/java/com/mycompany/api/modelo/Estadio.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.api.modelo;
 
-/**
- *
- * @author Alejandro
- */
 public class Estadio {
-    
+    private String nombre;
+    private String ciudad;
+    private int capacidad;
+
+    public Estadio(String nombre, String ciudad, int capacidad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.capacidad = capacidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
 }
