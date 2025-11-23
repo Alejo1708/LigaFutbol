077041f832cc92074f09e983311101ec9d69d3de
jcvmurillo12 <jcvmurillo12@gmail.com>
2025-11-22 20:49:40 -0500

    Update ServidorAPI.java
    subo nuevamente mi endpoint

--- API/src/main/java/com/mycompany/api/ServidorAPI.java
+++ API/src/main/java/com/mycompany/api/ServidorAPI.java
@@ -47,7 +47,53 @@ public class ServidorAPI {
             return ServidorUtils.toJson(lista);
         });
         
+// ---------------------------------------------------------
+        // ENDPOINT 2 jcvmurillo12: GET /equipos
+        // Devuelve los equipos CON SUS JUGADORES correspondientes
+        // ---------------------------------------------------------
+        Spark.get("/equipos", (req, res) -> {
+            res.type("application/json");
 
+            // Lista que se enviara como respuesta
+            List<Equipo> lista = new ArrayList<>();
+
+            // Recorrer la lista de nombres de equipos
+            for (String nombreEquipo : DatosLiga.EQUIPOS) {
+
+                // Lista donde se guardaran los jugadores de este equipo
+                List<Jugador> jugadoresDelEquipo = new ArrayList<>();
+
+                // Obtener los jugadores del mapa JUGADORES
+                List<String> listaTextoJugadores = DatosLiga.JUGADORES.get(nombreEquipo);
+
+                // Si el equipo si tiene jugadores definidos en DatosLiga
+                if (listaTextoJugadores != null) {
+                    // Convertir cada linea en un objeto Jugador
+                    for (String linea : listaTextoJugadores) {
+                    // Cada string tiene formato: "nombre, posicion, dorsal"
+                        String[] partes = linea.split(", ");
+
+                        String nombreJugador = partes[0];
+                        String posicion = partes[1];
+                        int dorsal = Integer.parseInt(partes[2]);
+
+                        // Crear objeto Jugador y agregarlo a la lista del equipo
+                        jugadoresDelEquipo.add(
+                            new Jugador(nombreJugador, posicion, dorsal)
+                        );
+                    }
+                }
+
+                // Crear el equipo con su lista de jugadores
+                Equipo equipo = new Equipo(nombreEquipo, jugadoresDelEquipo);
+
+                // Agregar a la lista final
+                lista.add(equipo);
+            }
+
+            // Convertir la lista completa a JSON
+            return ServidorUtils.toJson(lista);
+        });
 
 
 // ENDPOINT 3 jhosusruiz: GET /estadios
